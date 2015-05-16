f809a9d8cd02cf8b8c06d22847dec3e82331f678
Øistein Sørensen <sorenso@gmail.com>
2015-05-16 18:15:53 +0200

    Fixed cleanup script.

--- remove_all_packages.sh
+++ remove_all_packages.sh
@@ -4,17 +4,19 @@
 sudo service simple-blog stop
 
 sudo apt-get remove --purge nodejs && sudo apt-get remove --purge unzip && \
-sudo apt-get remove --purge puppet && sudo rm -rf /srv/ && sudo rm -rf /etc/puppet/
+sudo apt-get remove --purge puppet
+sudo rm -rf /srv/
+sudo mv /etc/puppet.orig/ /etc/puppet/
 
-sudo rm -rf /etc/puppet/ && sudo mv /etc/puppet.orig/ /etc/puppet/ && \
-sudo rm -rf /etc/simple-blog/ && sudo rm -f /etc/init/simple-blog.conf && sudo rm -f /etc/logrotate.d/simple-blog && \
-sudo rm -rf /var/log/simple-blog/ && sudo rm -f /var/log/upstart/simple-blog.log && \
+sudo rm -f /etc/init/simple-blog.conf
+sudo rm -f /etc/logrotate.d/simple-blog
+sudo rm -f /var/log/upstart/simple-blog.log
+
+sudo rm -rf /var/log/simple-blog/
 sudo rm -rf /var/run/simple-blog/
 
-
 # Check all files.
-sudo ps aux | grep node && sudo ls -al /srv/ /etc/simple-blog/ /etc/init/simple-blog.conf /etc/logrotate.d/simple-blog /var/log/simple-blog/ /etc/puppet/ /var/run/simple-blog/
-
+sudo ps aux | grep node && sudo ls -al /srv/ /etc/init/simple-blog.conf /etc/logrotate.d/simple-blog /var/log/simple-blog/ /etc/puppet/ /var/run/simple-blog/
 
 
 # Run user_data.sh
