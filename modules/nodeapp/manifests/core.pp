2119984608881d327e7c69a4fc8a28b2c67c4f3e
Øistein Sørensen <sorenso@gmail.com>
2015-05-16 14:46:54 +0200

    Take 7

--- modules/nodeapp/manifests/core.pp
+++ modules/nodeapp/manifests/core.pp
@@ -2,14 +2,14 @@ class nodeapp::core {
     notice("I am a node $::appname and want the software")
 
     exec { "nodeapp_download_config":
-        command => "git clone ${::gitconfigrepo} /srv/",
+        command => "git clone ${::gitconfigrepo} /srv/config/",
         path    => "/usr/local/bin/:/usr/bin/:/bin/",
         cwd     => "/srv/",
         require => Class["nodeapp::prework"]
     } ->
 
     exec { "nodeapp_download_src":
-        command => "git clone ${::gitrepo} /srv/",
+        command => "git clone ${::gitrepo} /srv/${::appname}/",
         path    => "/usr/local/bin/:/usr/bin/:/bin/",
         cwd     => "/srv/",
         require => Class["nodeapp::prework"]
